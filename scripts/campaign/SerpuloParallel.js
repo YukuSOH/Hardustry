f2ac0ddbb364363470d6c4b07cc48f60ed67b15e
YukuSOH <80609135+YukuSOH@users.noreply.github.com>
2022-11-26 09:07:43 +0900

    Delete SerpuloParallel

--- scripts/campaign/SerpuloParallel.js
+++
@@ -1,5 +0,0 @@
-const serpuloParallel = new JavaAdapter(Planet, {}, "serpulo-parallel", Planet.sun, 3, 1);
-serpuloParallel.generator = extend(SerpuloPlanetGenerator, {});
-module.exports = {
-    serpuloParallel: serpuloParallel
-}
