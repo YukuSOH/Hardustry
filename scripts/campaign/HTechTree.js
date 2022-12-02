5818f9fcfdf97813fa156edc9d521f3f6296de55
YukuSOH <80609135+YukuSOH@users.noreply.github.com>
2022-12-02 18:23:00 +0900

    Modify HTechTree.hjson

--- scripts/campaign/HTechTree.js
+++ scripts/campaign/HTechTree.js
@@ -8,26 +8,57 @@ function HBlock(str_name){
 function HItem(str_name){
     return Vars.content.item("hardustry-" + str_name);
 }
-// TechTree
+function HSector(str_name){
+    return Vars.content.sector("hardustry-" + str_name);
+}
 const SerpuloParallelTechTree = extend(SerpuloTechTree, {
     load(){
         Vars.content.planet("serpulo-parallel").techTree =
         nodeRoot("serpulo-parallel", Blocks.coreShard, ()=>{
+            // ProduceBlock
             TechTree.node(HBlock("quarry"), ()=>{
+                // Drill
                 TechTree.node(Blocks.mechanicalDrill);
+                // Energy
                 TechTree.node(HBlock("wind-turbine"), ()=>{
                     TechTree.node(Blocks.powerNode, ()=>{
                         TechTree.node(Blocks.battery);
                     });
                 });
             });
-            TechTree.node(Blocks.copperWall);
+            // Turret
+            TechTree.node(Blocks.duo, ()=>{
+                TechTree.node(Blocks.copperWall);
+            });
+            // Transfer
             TechTree.node(Blocks.conveyor, ()=>{
                 TechTree.node(Blocks.junction, ()=>{
                     TechTree.node(Blocks.router, ()=>{
                         TechTree.node(Blocks.unloader);
                     });
                 });
+            });
+            // Material
+            TechTree.node(Items.copper, ()=>{
+                // Liquids
+                // Misc
+                // Metals
+                TechTree.nodeProduce(Items.lead, ()=>{
+                    TechTree.nodeProduce(Items.coal, ()=>{});
+                    TechTree.nodeProduce(HItem("zinc"), ()=>{
+                        TechTree.nodeProduce(HItem("brass"), ()=>{});
+                    });
+                    TechTree.nodeProduce(HItem("tin"), ()=>{
+                        TechTree.nodeProduce(HItem("bronze"), ()=>{});
+                    });
+                });
+                // Ores
+                TechTree.nodeProduce(HItem("teallite"), ()=>{
+                    TechTree.nodeProduce(HItem("tetrahedrite"), ()=>{
+                    });
+                });
+                // Products
+                TechTree.nodeProduce(HItem("mc"), ()=>{});
             });
         });
     }
