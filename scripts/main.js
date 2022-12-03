c1e9c63c98df3e80d70bf4d35fc0c3431ee174a5
YukuSOH <80609135+YukuSOH@users.noreply.github.com>
2022-12-03 14:17:32 +0900

    Modify main.js

--- scripts/main.js
+++ scripts/main.js
@@ -2,5 +2,6 @@ require("campaign/HTechTree");
 
 require("blocks/production/TierDrill");
 require("blocks/production/Furnace");
+require("blocks/production/RotateCrafter");
 require("blocks/storage/HCoreBlock");
 require("blocks/power/WindGenerator");
